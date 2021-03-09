9e6b09099b951599ea145ef183470a01730c1078
Chris Oliveira <Chris Oliveira>
2021-03-09 15:58:58 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Tmdb from './Tmdb';
 
-function App() {
+export default ( ) => {
+
+  useEffect(()=> {
+      const loadAll = async () =>{
+        //Pegando toda a Lista
+        let list = await Tmdb.getHomeList();
+        console.log(list);
+      }
+      loadAll();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      Olá mundo
+    
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/Tmdb.js
@@ -0,0 +1,58 @@
+const API_KEY = '23f88a55177e6f78d51ad7867850c23d';
+const API_BASE = 'https://api.themoviedb.org/3';
+
+
+const basicFetch = async(endpoint) => {
+    const req = await fetch(`${API_BASE}${endpoint}`);
+    const json = await req.json();
+    return json;
+}
+
+export default {
+    getHomeList: async() =>{
+        return [
+            {
+                slug: 'originals',
+                title: 'Originais do Netflix',
+                items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'trengind',
+                title:'Recomendados para Você',
+                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'toprated',
+                title:' Em Alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'action',
+                title:'Ação',
+                items: await basicFetch(`/discover/movie?witch_genres=28&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'Comedy',
+                title:'Comédia',
+                items: await basicFetch(`/discover/movie?witch_genres=35&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'horror',
+                title:'Terror',
+                items: await basicFetch(`/discover/movie?witch_genres=27&language=pt-BR&api_key=${API_KEY}`)
+            },
+            {
+                slug: 'romance',
+                title:' Romance ',
+                items: await basicFetch(`/discover/movie?witch_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+            },
+
+            {
+                slug: 'documentary',
+                title:' Documentários',
+                items: await basicFetch(`/discover/movie?witch_genres=99&language=pt-BR&api_key=${API_KEY}`)
+            },
+        ];
+        
+    }
+}
